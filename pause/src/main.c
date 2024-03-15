de0661d01cb218d270c84a95aa89ffba0687775a
badre2911 <mistergeeks@gmail.com>
2024-03-15 21:04:30 +0100

    Sources files

---
+++ pause/src/main.c
@@ -0,0 +1,13 @@
+/*
+ Title:		main.c
+ Author:	Badre
+ Created:	20/02/2024
+*/
+#include <stdio.h>
+#include <mos_api.h>
+
+int main() {
+    printf("Press any key to continue ...\r\n");
+    getch();
+    return 0;
+}
